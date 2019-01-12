ef82e21ad2ac02f90421edf579721d8ba8de5ca3
Rachel Grier <rgrier104@gmail.com>
2019-01-12 21:06:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,10 @@
 class Student
 
+  def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
+  
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
